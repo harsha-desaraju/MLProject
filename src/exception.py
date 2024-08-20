124b80918ee9f3a1edcb3bbb69a7649437ac207d
harsha-desaraju <desaraju.harsha@gmail.com>
2024-08-20 23:41:06 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -8,14 +8,11 @@ def error_message_details(error, error_detail:sys):
     error_message = "Error occured in python script name [{0}]  line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, error
     )
-    print(error_message)
-
     return error_message
 
 
 class CustomException(Exception):
     def __init__(self, error, error_detail:sys):
-        print(1)
         super().__init__(error)
         self.error_message = error_message_details(
             error,
@@ -24,13 +21,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-
-
-if __name__ == '__main__':
-
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e, sys)
